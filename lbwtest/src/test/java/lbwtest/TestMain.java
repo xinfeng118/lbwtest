6d13b59227ec8f04617ea0615618088dde5bf9db
I074115 <I074115@PVGN50909619A.apj.global.corp.sap>
2017-06-09 23:45:47 +0800

    mine branch

--- lbwtest/src/test/java/lbwtest/TestMain.java
+++ lbwtest/src/test/java/lbwtest/TestMain.java
@@ -5,6 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("branch dev");
+       System.out.println("branch mine");
 	}
 
 }
