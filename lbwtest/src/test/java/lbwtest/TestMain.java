24c0afcba3c6e7947fde8b16e5b9b1c079987139
I074115 <I074115@PVGN50909619A.apj.global.corp.sap>
2017-06-10 09:38:20 +0800

    code 1 for V3

--- lbwtest/src/test/java/lbwtest/TestMain.java
+++ lbwtest/src/test/java/lbwtest/TestMain.java
@@ -3,8 +3,8 @@ package lbwtest;
 public class TestMain {
 
 	public static void main(String[] args) {
-       System.out.println("V2");
-       System.out.println("code change 3 for v2");
+       System.out.println("V3");
+       System.out.println("code change 1 for v3");
 	}
 
 }
