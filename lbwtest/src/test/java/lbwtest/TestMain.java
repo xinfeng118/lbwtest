51e4ddbe8834d11bbc9551df4dfc4aeaec0e1468
I074115 <I074115@PVGN50909619A.apj.global.corp.sap>
2017-06-10 09:27:50 +0800

    code change 1 for v2

--- lbwtest/src/test/java/lbwtest/TestMain.java
+++ lbwtest/src/test/java/lbwtest/TestMain.java
@@ -3,8 +3,8 @@ package lbwtest;
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("V1");
+       System.out.println("V2");
+       System.out.println("code change for v2");
 	}
 
 }
