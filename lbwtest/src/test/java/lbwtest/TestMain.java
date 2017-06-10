441f3bb22372884a0f77e95d64087d0068224e24
I074115 <I074115@PVGN50909619A.apj.global.corp.sap>
2017-06-10 09:48:42 +0800

    reset

--- lbwtest/src/test/java/lbwtest/TestMain.java
+++ lbwtest/src/test/java/lbwtest/TestMain.java
@@ -1,9 +1,10 @@
 package lbwtest;
 
 public class TestMain {
+
 	public static void main(String[] args) {
-       System.out.println("V2");
-       System.out.println("code change 3 for v2");
+       System.out.println("V3");
+       System.out.println("code change 4 for v3");
 	}
 
 }
