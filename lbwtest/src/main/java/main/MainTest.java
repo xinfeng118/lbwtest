b7d8c393ad66c0d1732b4f9b0b7c2a43737745d3
I074115 <I074115@PVGN50909619A.apj.global.corp.sap>
2017-06-26 11:01:23 +0800

    sdsd

---
+++ lbwtest/src/main/java/main/MainTest.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Locale;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Locale aa = Locale.CHINA;
+       System.out.println(aa);
+	}
+
+}
--- lbwtest/src/test/java/lbwtest/TestMain.java
+++ lbwtest/src/test/java/lbwtest/TestMain.java
@@ -3,7 +3,7 @@ package lbwtest;
 public class TestMain {
 
 	public static void main(String[] args) {
-       System.out.println("V4:-2");
+       System.out.println("V4:-0-");
 	}
 
 }
